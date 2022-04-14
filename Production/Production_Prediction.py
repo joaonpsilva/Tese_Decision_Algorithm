bc2678cd1540a998611c9f3ae78688f77b2f8f7c
joaonpsilva <joao.npsilva@ua.pt>
2022-04-14 20:08:51 +0100

    production

---
+++ Production/Production_Prediction.py
@@ -0,0 +1,68 @@
+import pandas as pd
+
+def removeSecs(a):
+    return a[:13]
+
+class Production_Meter:
+
+    def __init__(self, startDate, solar_Panel_Area=1):
+        
+        self._solar_Panel_Area = solar_Panel_Area
+
+        self.df = pd.read_csv("London_Solar.csv", delimiter = ',')
+
+        self.df['PeriodStart'] = self.df.apply(lambda row : removeSecs(row['PeriodStart']), axis = 1)
+        self.df['PeriodEnd'] = self.df.apply(lambda row : removeSecs(row['PeriodEnd']), axis = 1)
+
+        self.df['PeriodStart']= pd.to_datetime(self.df['PeriodStart'], format='%Y-%m-%dT%H')
+        self.df['PeriodEnd']= pd.to_datetime(self.df['PeriodEnd'], format='%Y-%m-%dT%H')
+
+        mask = (self.df['PeriodStart'] >= startDate)
+        self.df = self.df.loc[mask]
+
+        self.i = 0
+    
+    @property
+    def solar_Panel_Area(self):
+        return self._solar_Panel_Area
+
+    @solar_Panel_Area.setter
+    def solar_Panel_Area(self, value):
+        self._solar_Panel_Area = value
+
+    def make_Prediction(self):
+        
+        #"irradiação normal direta no intervalo em watts por metro quadrado"
+        dni = self.df.iloc[self.i]["Dni"]
+        self.i += 1
+
+        #"valores mais comuns para a performance de células fotovoltaicas"
+        r = 0.15 
+
+        #"desprezável o fator de idade do painel, ou seja, não é considerada a deterioração do mesmo ao longo do tempo"
+        p = 0.75
+        E = self._solar_Panel_Area * r * dni * p
+
+        return E
+
+if __name__ == "__main__":
+    from datetime import datetime, timedelta
+
+
+    timestamp = "2012-10-20T09:00:00Z"
+    timestamp = datetime.strptime(timestamp[:13], '%Y-%m-%dT%H')
+    production_Meter = Production_Meter(timestamp)
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+    print(production_Meter.make_Prediction())
+
+
