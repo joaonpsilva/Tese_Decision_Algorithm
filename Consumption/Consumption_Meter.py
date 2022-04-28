5b5e95b8125947c2f4b46062afa995048978cbb5
joaonpsilva <joao.npsilva@ua.pt>
2022-04-28 18:51:22 +0100

    filename parameter

--- Consumption/Consumption_Meter.py
+++ Consumption/Consumption_Meter.py
@@ -3,12 +3,16 @@ import os
 from datetime import datetime
 
 class Consumption_Meter:
-    def __init__(self) -> None:
-        directory = "../../datsets/LCL_Data_Transformed/test"
+    def __init__(self, filename = None) -> None:
 
-        filename = sorted(os.listdir(directory))[0]
-        f = os.path.join(directory, filename)
-        self.consumptionDF = pd.read_csv(f, delimiter = ',',index_col=0)
+        if filename:
+            self.consumptionDF = pd.read_csv(filename, delimiter = ',',index_col=0)
+        else:
+            directory = "../datsets/LCL_Data_Transformed_final/test"
+
+            filename = sorted(os.listdir(directory))[0]
+            f = os.path.join(directory, filename)
+            self.consumptionDF = pd.read_csv(f, delimiter = ',',index_col=0)
 
         self.index=-1
 
