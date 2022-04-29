4782c617eb2b893be57cb74c92d9beae81198b75
joaonpsilva <joao.npsilva@ua.pt>
2022-04-29 20:42:08 +0100

    decision funtion

--- decisionAlg.py
+++ decisionAlg.py
@@ -21,7 +21,7 @@ class Decision_Alg:
     
     def define_receive_Priority(self):
         
-        self.receive_Priority.append(Priority_Object(Consumption, 0, 0))
+        self.receive_Priority.append(Priority_Object("Consumption", 0, 0))
 
         needing_charge = [ev for ev in self.connected_EVS if ev.soc < ev.batterry_Threshold]
         needing_charge.sort(key=lambda x: x.departure_Time)
@@ -42,7 +42,7 @@ class Decision_Alg:
                 self.receive_Priority.append(Priority_Object(ev, 2, 0))
     
     def define_give_Priority(self):
-        self.give_Priority.append(Priority_Object(Production, 0, 0))
+        self.give_Priority.append(Priority_Object("Production", 0, 0))
         dispending_charge = [ev for ev in self.connected_EVS if ev.soc > ev.batterry_Threshold]
         dispending_charge.sort(key=lambda x: x.soc - x.batterry_Threshold)
 
@@ -52,45 +52,48 @@ class Decision_Alg:
         
         self.give_Priority.append(Priority_Object(self.stationary_Battery, 2, 0))
 
-        self.give_Priority.append(Priority_Object(Grid, 3, 0))
+        self.give_Priority.append(Priority_Object("Grid", 3, 0))
 
     
     def make_Decisions(self):
 
-        receive_Priority_0_kw = sum([x.amount_kw for x in self.receive_Priority if x.priority == 0])
-        give_Priority_0_kw = sum([x.amount_kw for x in self.give_Priority if x.priority == 0])
 
+        decisions = []
+        self.give_Priority = [obj for obj in self.give_Priority if obj.amount_kw > 0]
 
-        
         for obj_receive in self.receive_Priority:
 
 
             if obj_receive.priority != 0:
                 give_Priority_0_kw = sum([x.amount_kw for x in self.give_Priority if x.priority == 0])
-                if give_Priority_0_kw > 0:
-                    #GO
-                    pass
-            
-            if obj_receive.priority == 0:
-                Decision(obj_receive, "receive", obj_receive.amount_kw)
-
-                for obj_give in self.give_Priority:
+                
+                #object is not priority and no availabe
+                if give_Priority_0_kw <= 0:
+                    break
+                
+                priority_flag = False #only receive from priority give
+                will_receive = obj_receive.amount_kw if give_Priority_0_kw >= obj_receive.amount_kw else give_Priority_0_kw
+            else:
+                priority_flag = True
+                will_receive = obj_receive.amount_kw
                     
-                    obj_give.amount_kw -= obj_receive.amount_kw
-                    if obj_give.amount_kw > 0: #can give more
-                        Decision(obj_give, "give", obj_receive.amount_kw)
-                        pass
-                    else:
-                        #CHECK IF CAN DO THIS
-                        Decision(obj_give, "give", obj_give.amount_kw)
-                        self.give_Priority.remove(obj_give)
+            decisions.append(Decision(obj_receive, "receive", will_receive))
 
+            #Find who will give energy
+            for obj_give in self.give_Priority:
+
+                if not priority_flag and obj_give.priority != 0:
+                    break
+                
+                obj_give.amount_kw -= will_receive
+                if obj_give.amount_kw >= 0: #can give more, receiver is satisfied
+                    decisions.append(Decision(obj_give, "give", will_receive))
+                    break
+                else:   #giver doesnt have enough, receiver is not satisfied
+                    #CHECK IF CAN DO THIS
+                    decisions.append(Decision(obj_give, "give", obj_give.amount_kw))
                     
-                    obj_receive.amount_kw -= obj_give.amount_kw
-                    if obj_receive.amount_kw > 0: #need more
-                        continue
-                    else:
-                        break
+
                         
                     
 
