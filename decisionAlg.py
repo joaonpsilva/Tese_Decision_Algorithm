9c875f2085a88f42b5a9a35c4a408f6cd126374b
joaonpsilva <joao.npsilva@ua.pt>
2022-05-01 21:56:00 +0100

    priority best defined

--- decisionAlg.py
+++ decisionAlg.py
@@ -1,7 +1,3 @@
-from EV import EV
-from Stationary_Battery import Stationary_Battery
-from Consumption.Consumption_Prediction import Consumption_Prediction
-from Production.Production_Prediction import Production_Meter
 from datetime import timedelta
 from Decision import Decision
 from Priority_Object import Priority_Object
@@ -17,112 +13,133 @@ class Decision_Alg:
     
     def analyse(self, context):
 
-
         expected_error = 1
-        expected_remaining =  context.pruction - (context.consumption + expected_error)
+        expected_remaining =  context.production - (context.consumption + expected_error)
 
-        self.define_receive_Priority(context.current_Time,
-                                       expected_remaining,
-                                       context.stationary_Battery)
-
-        self.define_give_Priority(expected_remaining,
-                                       context.stationary_Battery)
-        
-        self.make_Deciosions()
+        self.define_receive_Priority(context, expected_remaining)
+        self.define_give_Priority(context, expected_remaining)
+        self.make_Decisions()
 
     
-    def define_receive_Priority(self, current_Time, expected_remaining, stationary_Battery):
+    def define_receive_Priority(self, context, expected_remaining):
 
         #ENERGY CONSUMPTION    
-        self.receive_Priority.append(Priority_Object("Consumption", 0, -1))
+        self.receive_Priority.append(Priority_Object("Consumption", 3, context.consumption))
 
         #EVS WITH LOW BATTERY
         needing_charge = [ev for ev in self.connected_EVS if ev.soc < ev.batterry_Threshold]
         needing_charge.sort(key=lambda x: x.departure_Time)
+        append_later = []
         for ev in needing_charge:
-            charge_needed = ev.batterry_Threshold - ev.soc
-            time_diff = ev.departure_Time - current_Time
+            charge_needed = ev.batterry_ThresholdKWH() - ev.socKWH()
+            time_diff = ev.departure_Time - context.current_Time
             time2charge = charge_needed / ev.charge_Rate
 
             if time2charge - time_diff < timedelta(hours=2):
-                self.receive_Priority.append(Priority_Object(ev, 0, charge_needed))
+                self.receive_Priority.append(Priority_Object(ev, 3, charge_needed))
             else:
-                self.receive_Priority.append(Priority_Object(ev, 1, charge_needed))
+                append_later.append(Priority_Object(ev, 2, charge_needed))
 
         #STATIONARY BATTERY
-        if expected_remaining < 0 and stationary_Battery.current_Capacity < -expected_remaining: #house will need energy
-            energy_needed = expected_remaining - stationary_Battery.current_Capacity
-            self.receive_Priority.append(Priority_Object(stationary_Battery, 1, energy_needed))
+        if expected_remaining < 0:#house will need energy
+            if context.stationary_Battery.current_Capacity < -expected_remaining * 2:#battery does not contain a lot of energy
+                
+                total_dispending = 0
+                vehicles_leaving_soon = 0
+                for ev in self.connected_EVS:   
+                    if ev not in needing_charge:    #for evs dispending energy
+                        time_diff = ev.departure_Time - context.current_Time
+                       
+                        #EV wont leave soon
+                        if time_diff >= timedelta(hours=1):     
+                            charge_dispending = ev.socKWH() - ev.batterry_ThresholdKWH()
+                            total_dispending += charge_dispending
+                        else:
+                            vehicles_leaving_soon += 1
+                            
+                if total_dispending < -expected_remaining and vehicles_leaving_soon > 0:
+                    energy_needed = (-expected_remaining * 2) - context.stationary_Battery.current_Capacity
+                    free_battery_space = context.stationary_Battery.max_Capacity - context.stationary_Battery.current_Capacity
+                    e = energy_needed if energy_needed < free_battery_space else free_battery_space
+                    
+                    #1.5 priority -> evs leaving soon
+                    self.receive_Priority.append(Priority_Object(context.stationary_Battery, 1.5, e))
         
-        self.receive_Priority.append(Priority_Object(stationary_Battery, 2, -1))
+        #Other EVS
+        self.receive_Priority += append_later
+        
+        #STATIONARY BATTERY
+        free_battery_space = context.stationary_Battery.max_Capacity - context.stationary_Battery.current_Capacity
+        self.receive_Priority.append(Priority_Object(context.stationary_Battery, 1, free_battery_space))
 
         #OTHER EVS
         for ev in self.connected_EVs:
             if ev not in needing_charge:
-                self.receive_Priority.append(Priority_Object(ev, 2, -1))
+                free_space = ev.battery_size - ev.socKWH()
+                self.receive_Priority.append(Priority_Object(ev, 1, free_space))
+        
+        self.receive_Priority.append(Priority_Object("Grid", 0, 99999999))
+
+
+
     
-    def define_give_Priority(self, expected_remaining, stationary_Battery):
+    def define_give_Priority(self, context, expected_remaining):
 
         #PRODUCTION
-        self.give_Priority.append(Priority_Object("Production", 0, -1))
+        self.give_Priority.append(Priority_Object("Production", 0, context.production))
+
+        #STATIONARY BATTERY SPECIAL CASE
+        free_space_battery = context.stationary_Battery.max_Capacity - context.stationary_Battery.current_Capacity
+        if expected_remaining > 0 and free_space_battery < expected_remaining: #house will produce more energy
+            energy_dispending = expected_remaining - free_space_battery
+            self.receive_Priority.append(Priority_Object(context.stationary_Battery, 1, energy_dispending))
         
         #EVS with more charge
         dispending_charge = [ev for ev in self.connected_EVS if ev.soc > ev.batterry_Threshold]
-        dispending_charge.sort(key=lambda x: x.soc - x.batterry_Threshold)
+        dispending_charge.sort(key=lambda x: x.departure_Time)  #usar os q vao sair primeiro?
         for ev in dispending_charge:
-            charge_dispendable = ev.soc - ev.batterry_Threshold
-            self.give_Priority.append(Priority_Object(ev, 1, charge_dispendable))
+            charge_dispendable = ev.socKWH() - ev.batterry_ThresholdKWH()
+            time_diff = ev.departure_Time - context.current_Time
+            
+            if time_diff >= timedelta(hours=1):     
+                self.give_Priority.append(Priority_Object(ev, 1.5, charge_dispendable))
+            
+            self.give_Priority.append(Priority_Object(ev, 2, charge_dispendable))
         
         #STATIONARY BATTERY
-        free_space_battery = stationary_Battery.max_Capacity - stationary_Battery.current_Capacity
-        if expected_remaining > 0 and free_space_battery < expected_remaining: #house will need energy
-            energy_dispending = expected_remaining - free_space_battery
-            self.receive_Priority.append(Priority_Object(stationary_Battery, 1, energy_dispending))
-        
-        self.receive_Priority.append(Priority_Object(stationary_Battery, 2, -1))
+        self.receive_Priority.append(Priority_Object(context.stationary_Battery, 2, context.stationary_Battery.current_Capacity))
 
         #GRID
-        self.give_Priority.append(Priority_Object("Grid", 3, -1))
+        self.give_Priority.append(Priority_Object("Grid", 3, 99999999))
 
-    
+
+
     def make_Decisions(self):
 
-
         decisions = []
-        self.give_Priority = [obj for obj in self.give_Priority if obj.amount_kw > 0]
 
         for obj_receive in self.receive_Priority:
+            
+            #remove givers that were exausted in previous loop
+            self.give_Priority = [obj for obj in self.give_Priority if obj.amount_kw > 0]
 
+            for obj_give in self.give_Priority: #Find who will give energy
 
-            if obj_receive.priority != 0:
-                give_Priority_0_kw = sum([x.amount_kw for x in self.give_Priority if x.priority == 0])
-                
-                #object is not priority and no availabe
-                if give_Priority_0_kw <= 0:
-                    break
-                
-                priority_flag = False #only receive from priority give
-                will_receive = obj_receive.amount_kw if give_Priority_0_kw >= obj_receive.amount_kw else give_Priority_0_kw
-            else:
-                priority_flag = True
-                will_receive = obj_receive.amount_kw
-                    
-            decisions.append(Decision(obj_receive, "receive", will_receive))
+                if obj_give.priority <= obj_receive.priority:
 
-            #Find who will give energy
-            for obj_give in self.give_Priority:
+                    obj_give.amount_kw -= obj_receive.amount_kw
 
-                if not priority_flag and obj_give.priority != 0:
-                    break
-                
-                obj_give.amount_kw -= will_receive
-                if obj_give.amount_kw >= 0: #can give more, receiver is satisfied
-                    decisions.append(Decision(obj_give, "give", will_receive))
-                    break
-                else:   #giver doesnt have enough, receiver is not satisfied
-                    #CHECK IF CAN DO THIS
-                    decisions.append(Decision(obj_give, "give", obj_give.amount_kw))
-                    
+                    if obj_give.amount_kw >= 0: #can give more, receiver is satisfied
+                        decisions.append(Decision(obj_receive, "receive", obj_receive.amount_kw))
+                        decisions.append(Decision(obj_give, "give", obj_receive.amount_kw))
+                        break
+                    else:   #giver doesnt have enough, receiver is not satisfied
+                        #CHECK IF CAN DO THIS
+                        decisions.append(Decision(obj_receive, "receive", obj_give.amount_kw))
+                        decisions.append(Decision(obj_give, "give", obj_give.amount_kw))
+                        obj_receive.amount_kw -= obj_give.amount_kw
+
+            
 
 
 #https://www.directenergyregulatedservices.com/blog/kw-vs-kwh-whats-difference
