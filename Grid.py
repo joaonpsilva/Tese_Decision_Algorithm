e6db75adea5806390e8ab2cb637cdfb88eebe6d5
joaonpsilva <joao.npsilva@ua.pt>
2022-05-03 19:14:33 +0100

    compare cost with grid

---
+++ Grid.py
@@ -0,0 +1,7 @@
+class Grid_Linear:
+    def __init__(self) -> None:
+        pass
+
+    @property
+    def kwh_price(self):
+        return 0.2
--- decisionAlg.py
+++ decisionAlg.py
@@ -29,14 +29,14 @@ class Decision_Alg:
         self.receive_Priority.append(Priority_Object("Consumption", 3, context["consumption"]))
 
         #EVS WITH LOW BATTERY
-        needing_charge = [ev for ev in context["connected_EVs"] if ev.soc < ev.batterry_Threshold]
+        needing_charge = [ev for ev in context["connected_EVs"] if ev.battery.soc < ev.batterry_Threshold]
         needing_charge.sort(key=lambda x: x.departure_Time)
         append_later = []
         for ev in needing_charge:
-            charge_needed = ev.batterry_ThresholdKWH() - ev.socKWH()
+            charge_needed = ev.batterry_ThresholdKWH - ev.battery.current_Capacity
             time_diff = ev.departure_Time - context["current_Time"]
             
-            time2charge = charge_needed / ev.charge_Rate
+            time2charge = charge_needed / ev.battery.charge_Rate
             h = int(time2charge)
             m = time2charge - h
             time2charge = timedelta(hours=h, minutes=60*m)
@@ -61,14 +61,14 @@ class Decision_Alg:
                        
                         #EV wont leave soon
                         if time_diff >= timedelta(hours=2):     
-                            charge_dispending = ev.socKWH() - ev.batterry_ThresholdKWH()
+                            charge_dispending = ev.battery.current_Capacity - ev.batterry_ThresholdKWH
                             total_dispending += charge_dispending
                         else:
                             vehicles_leaving_soon += 1
                             
                 if total_dispending < -expected_remaining and vehicles_leaving_soon > 0:
                     energy_needed = (-expected_remaining * 2) - context["stationary_Battery"].current_Capacity
-                    free_battery_space = context["stationary_Battery"].max_Capacity - context["stationary_Battery"].current_Capacity
+                    free_battery_space = context["stationary_Battery"].battery_size - context["stationary_Battery"].current_Capacity
                     e = energy_needed if energy_needed < free_battery_space else free_battery_space
                     
                     #1.5 priority -> evs leaving soon
@@ -76,13 +76,13 @@ class Decision_Alg:
         
         
         #STATIONARY BATTERY
-        free_battery_space = context["stationary_Battery"].max_Capacity - context["stationary_Battery"].current_Capacity
+        free_battery_space = context["stationary_Battery"].battery_size - context["stationary_Battery"].current_Capacity
         self.receive_Priority.append(Priority_Object(context["stationary_Battery"], 1, free_battery_space))
 
         #OTHER EVS
         for ev in context["connected_EVs"]:
             if ev not in needing_charge:
-                free_space = ev.battery_size - ev.socKWH()
+                free_space = ev.battery.battery_size - ev.battery.current_Capacity
                 self.receive_Priority.append(Priority_Object(ev, 1, free_space))
         
         self.receive_Priority.append(Priority_Object("Grid", 0, 99999999))
@@ -95,29 +95,35 @@ class Decision_Alg:
         #PRODUCTION
         self.give_Priority.append(Priority_Object("Production", 0, context["production"]))
 
+        #if grid really low price fill everything?
+
         #STATIONARY BATTERY SPECIAL CASE
-        free_space_battery = context["stationary_Battery"].max_Capacity - context["stationary_Battery"].current_Capacity
+        free_space_battery = context["stationary_Battery"].battery_size - context["stationary_Battery"].current_Capacity
         if expected_remaining > 0 and free_space_battery < expected_remaining: #house will produce more energy
             energy_dispending = expected_remaining - free_space_battery
             self.give_Priority.append(Priority_Object(context["stationary_Battery"], 1, energy_dispending))
         
         #EVS with more charge
-        dispending_charge = [ev for ev in context["connected_EVs"] if ev.soc > ev.batterry_Threshold]
+        dispending_charge = [ev for ev in context["connected_EVs"] if ev.battery.soc > ev.batterry_Threshold and ev.battery.kwh_price < context["grid"].kwh_price]
+
         dispending_charge.sort(key=lambda x: x.departure_Time)  #usar os q vao sair primeiro?
         for ev in dispending_charge:
-            charge_dispendable = ev.socKWH() - ev.batterry_ThresholdKWH()
+            charge_dispendable = ev.battery.current_Capacity - ev.batterry_ThresholdKWH
             time_diff = ev.departure_Time - context["current_Time"]
             
             if time_diff >= timedelta(hours=1):     
                 self.give_Priority.append(Priority_Object(ev, 1.5, charge_dispendable))
-            
-            self.give_Priority.append(Priority_Object(ev, 2, charge_dispendable))
+            else:
+                self.give_Priority.append(Priority_Object(ev, 2, charge_dispendable))
         
         #STATIONARY BATTERY
-        self.give_Priority.append(Priority_Object(context["stationary_Battery"], 2, context["stationary_Battery"].current_Capacity))
+        grid_P = 2
+        if context["stationary_Battery"].kwh_price < context["grid"].kwh_price:
+            self.give_Priority.append(Priority_Object(context["stationary_Battery"], 2, context["stationary_Battery"].current_Capacity))
+            grid_P = 3
 
         #GRID
-        self.give_Priority.append(Priority_Object("Grid", 3, 99999999))
+        self.give_Priority.append(Priority_Object("Grid", grid_P, 99999999))
 
 
 
@@ -125,8 +131,9 @@ class Decision_Alg:
 
         decisions = []
         
-        print(self.receive_Priority)
-        print(self.give_Priority)
+        print("RECEIVE: ", self.receive_Priority)
+        print()
+        print("GIVE: ", self.give_Priority)
 
         for obj_receive in self.receive_Priority:
             
