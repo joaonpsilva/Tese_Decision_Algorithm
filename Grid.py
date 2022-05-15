5b2c91e4c9cee5153375412c4a7da1bd96f95254
joaonpsilva <joao.npsilva@ua.pt>
2022-05-15 22:12:38 +0100

    fix grid dynamioc

--- Grid.py
+++ Grid.py
@@ -25,8 +25,8 @@ class Grid_sinusoidal:
 
     @property
     def kwh_price(self):
-        return 0.2 * (math.cos( (self.hour/4) - 11) + 1) + 0.1
+        return 0.15 * (math.cos( (self.hour/4) - 10.5) + 1) + 0.1
     
     @property
     def average_kwh_price(self):
-        return 0.3
+        return 0.25
