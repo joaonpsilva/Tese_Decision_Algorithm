a6fa6fa06547f669e13aa13a401bc7d96eac644c
joaonpsilva <joao.npsilva@ua.pt>
2022-05-15 22:12:27 +0100

    better arrival mechanism and better values

--- EV/EV_Garage.py
+++ EV/EV_Garage.py
@@ -3,6 +3,10 @@ import random
 import numpy as np
 from datetime import timedelta, datetime
 
+
+
+
+
 class EV_Garage:
     def __init__(self, max_Evs) -> None:
         self.max_Evs = max_Evs
@@ -10,7 +14,9 @@ class EV_Garage:
         self.parked_Evs = []
         self.away_Evs = [EV() for i in range(max_Evs)]
 
-        self.commonArrivalHours = [12, 13, 17, 18, 21, 22]
+        self.commonArrivalHours = [11,12,13,18,19,20,22,23]
+
+        self.leave_hours = [8, 14, 20]
         self.evArrival_Prob = 0.1 
 
         self.empaired_evs = 0
@@ -26,7 +32,6 @@ class EV_Garage:
                 #print("EV LEAVING: " + str(ev_info)) 
                 if ev_info["ev"].battery.soc < ev_info["ev"].batterry_Threshold:
                     self.empaired_evs += 1
-                    print("EMPAIRED", ev_info)
 
                 self.away_Evs.append(ev_info["ev"])
 
@@ -50,28 +55,44 @@ class EV_Garage:
                 p += 0.15
         
             if random.random() < p: #CAR ARRIVED
-
                 ev = random.choice(self.away_Evs) #get random vehicle that was away
                 self.away_Evs.remove(ev)
 
-                charged_soc = round(np.random.triangular(0, 0.2, 0.8, size=None),2) #ev might have charged
-                ev.battery.soc = ev.battery.soc - (ev.batterry_Threshold * random.uniform(0.8, 0.99)) + charged_soc #previous soc - soc needed for previous trip + charged
+                #SOC
+                charged_soc = round(np.random.triangular(0, 0.2, 0.6, size=None),2) #ev might have charged
+                ev.battery.soc = ev.battery.soc - (ev.batterry_Threshold * random.uniform(0.5, 1)) + charged_soc #previous soc - soc needed for previous trip + charged
 
+                #DEPARTURE TIME
+                choosen_hour = random.choice(self.leave_hours)
+                choosen_hour = round(random.gauss(choosen_hour, 2))
+                choosen_hour = 0 if choosen_hour < 0 else choosen_hour
+                choosen_hour = choosen_hour - 24 if choosen_hour > 23 else choosen_hour
 
-                departure_Time = round(np.random.triangular(1, 3, 24, size=None))   #new departure time
-                sigma = departure_Time/4 #quanto mais pequeno departure mais certeza
-                sigma = min([sigma,1.5])
-                departure_guess = round(random.gauss(departure_Time, sigma))
+                choosen_date = current_Date
+
+                if choosen_hour <= choosen_date.hour:
+                    choosen_date += timedelta(days=1)
+                choosen_date = choosen_date.replace(hour=choosen_hour)
+
+                
+                #DEPARTURE GUESS
+                time_left = choosen_date - current_Date
+                time_left = int(time_left.total_seconds() / 3600)
+
+                sigma = min([time_left/4 ,1.5]) #quanto mais pequeno departure mais certeza
+                departure_guess = round(random.gauss(time_left, sigma))
                 departure_guess = 1 if departure_guess < 1 else departure_guess
 
                 ev.departure_Time = current_Date + timedelta(hours=departure_guess)
 
-                max_T = min([5/24 * departure_Time + ev.battery.soc, 0.8])
+
+                #THRESHOLD
+                max_T = min([5/24 * time_left + ev.battery.soc, 0.8])
                 mid = 0.15 if max_T <= 0.3 else 0.3
                     
                 ev.batterry_Threshold = round(np.random.triangular(0.1, mid, max_T, size=None),2) #new battery threshold
                                 
-                ev_info = {"dep_Time":departure_Time, "ev": ev}
+                ev_info = {"dep_Time":time_left, "ev": ev}
                 self.parked_Evs.append(ev_info)
 
                 #print("EV ARRIVED: " + str(ev_info)) 
