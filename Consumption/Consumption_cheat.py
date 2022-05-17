328eb9669e502ea9b962cf85b62543c6f4c399fa
joaonpsilva <joao.npsilva@ua.pt>
2022-05-17 21:07:39 +0100

    faster

---
+++ Consumption/Consumption_cheat.py
@@ -0,0 +1,18 @@
+class Consumption_Cheat:
+    def __init__(self, past_window, fileNumber) -> None:
+
+        self.f = open("cheat" + str(fileNumber) + ".txt", "r")
+        self.i = 0
+        self.past_window = past_window
+    
+    def new_Record(self, record):
+        pass
+
+    def get_Prediction(self):
+        self.i+=1
+        if self.i <= self.past_window:
+            return None
+        
+        return float(self.f.readline())
+        
+        
